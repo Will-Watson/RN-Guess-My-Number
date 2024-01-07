5d9267a8c9a4eb4710f0bed09eb0bffacef99092
William Watson <williampwatson1991@gmail.com>
2024-01-07 18:36:19 -0500

    feat: Dummy component structure added to StartGameScreen

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { TextInput, Pressable, View } from "react-native";
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+      <Pressable />
+    </View>
+  );
+};
+
+export default StartGameScreen;
