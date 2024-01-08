0989c65157123f913f2265ab8196525618ae19d6
William Watson <williampwatson1991@gmail.com>
2024-01-07 22:42:35 -0500

    feat: added some dummy data to the gamescreen component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,24 @@
-import { Text } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const GameScreen = () => {
-  return <Text>Game Screen!</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <View>
+        <Text>Higher or Lower?</Text>
+      </View>
+      <View>
+        {/* LOG ROUNDS */}
+        </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
 
 export default GameScreen;
