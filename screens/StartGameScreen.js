e72d2757e93d45e6cb221f9f8c363963282b6d7d
William Watson <williampwatson1991@gmail.com>
2024-01-07 23:03:34 -0500

    feat: created a global colors file and imported in necessary places

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { TextInput, View, StyleSheet, Alert } from 'react-native';
 
 import PrimaryButton from '../components/PrimaryButton';
+import Colors from '../constants/colors';
 
 const StartGameScreen = ({ onPickNumber }) => {
   const [enteredNumber, setEnteredNumber] = useState('');
@@ -65,7 +66,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     borderRadius: 8,
-    backgroundColor: '#3b021f',
+    backgroundColor: Colors.primary800,
     elevation: 4,
     shadowColor: 'black',
     shadowOffset: { width: 0, height: 2 },
@@ -82,9 +83,9 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     fontSize: 32,
-    borderBottomColor: '#ddb52f',
+    borderBottomColor: Colors.accent500,
     borderBottomWidth: 2,
-    color: '#ddb52f',
+    color: Colors.accent500,
     marginVertical: 8,
     fontWeight: 'bold',
     textAlign: 'center',
