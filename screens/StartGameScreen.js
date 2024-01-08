92b4cf4fbfdf24ad40e17269f9cb7faacba6f9e6
William Watson <williampwatson1991@gmail.com>
2024-01-07 20:40:34 -0500

    feat: onPress dummy logic added to first button, confirm

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,8 +1,19 @@
+import { useState } from 'react';
 import { TextInput, View, StyleSheet } from 'react-native';
 
 import PrimaryButton from '../components/PrimaryButton';
 
 const StartGameScreen = () => {
+  const [enteredNumber, setEnteredNumber] = useState('');
+
+  const numberInputHandler = (enteredText) => {
+    setEnteredNumber(enteredText);
+  };
+
+  const confirmInputHandler = () => {
+    
+  };
+
   return (
     <View style={styles.inputContainer}>
       <TextInput
@@ -11,13 +22,15 @@ const StartGameScreen = () => {
         keyboardType='number-pad'
         autoCapilalize='none'
         autoCorrect={false}
+        onChangeText={numberInputHandler}
+        value={enteredNumber}
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
           <PrimaryButton>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Confirm</PrimaryButton>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
         </View>
       </View>
     </View>
