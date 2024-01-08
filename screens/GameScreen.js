86d8afd13ca52593dded031bb43aedb5640e834b
William Watson <williampwatson1991@gmail.com>
2024-01-07 23:17:42 -0500

    feat: created number container component for reusability

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import Title from '../components/Title';
+import Title from '../components/ui/Title';
+import NumberContainer from '../components/game/NumberContainer';
 
-const GameScreen = () => {
+const generateRandomBetween = (min, max, exclude) => {
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+
+  if (randomNumber === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return randomNumber;
+  }
+};
+
+const GameScreen = ({ userNumber }) => {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or Lower?</Text>
       </View>
