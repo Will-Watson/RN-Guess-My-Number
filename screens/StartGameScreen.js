d4a416b7daefb31eae21bcf3b9674d66049f23eb
William Watson <williampwatson1991@gmail.com>
2024-01-07 20:49:17 -0500

    feat: button logic added for reset and confirm

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { TextInput, View, StyleSheet } from 'react-native';
+import { TextInput, View, StyleSheet, Alert } from 'react-native';
 
 import PrimaryButton from '../components/PrimaryButton';
 
@@ -10,8 +10,26 @@ const StartGameScreen = () => {
     setEnteredNumber(enteredText);
   };
 
+  const resetInputHandler = () => {
+    setEnteredNumber('');
+  };
+
   const confirmInputHandler = () => {
-    
+    const chosenNumber = parseInt(enteredNumber);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        'Invalid number!',
+        'Number has to be a number between 1 and 99.',
+        [
+          {
+            text: 'Okay',
+            style: 'destructive',
+            onPress: () => resetInputHandler,
+          },
+        ]
+      );
+      return;
+    }
   };
 
   return (
@@ -27,7 +45,7 @@ const StartGameScreen = () => {
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Reset</PrimaryButton>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
           <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
