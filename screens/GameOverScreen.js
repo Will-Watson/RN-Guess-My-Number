85d7d97709a11e6109fc1c728a818af21630fb1c
William Watson <williampwatson1991@gmail.com>
2024-01-11 18:52:10 -0500

    feat: further improvements with useWindowDimensions

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,31 +1,57 @@
-import { Text, StyleSheet, View, Image, Dimensions } from 'react-native';
+import { Text, StyleSheet, View, Image, useWindowDimensions, ScrollView } from 'react-native';
 
 import Title from '../components/ui/Title';
 import PrimaryButton from '../components/ui/PrimaryButton';
 import Colors from '../constants/colors';
 
 const GameOverScreen = ({roundsNumber, userNumber, onStartNewGame}) => {
+  const { width, height } = useWindowDimensions();
+
+  let imageSize = 300;
+
+  if (width < 380) {
+    imageSize = 150;
+  }
+
+  if (height <400) {
+    imageSize = 80;
+  }
+
+  const imageStyle = {
+    width: imageSize,
+    height: imageSize,
+    borderRadius: imageSize / 2,
+  }
+
+
+
   return (
+    <ScrollView style={styles.screen}>
     <View style={styles.rootContainer}>
       <Title>GAME OVER!</Title>
-      <View style={styles.imageContainer}>
+      <View style={[styles.imageContainer, imageStyle]}>
         <Image
           style={styles.image}
           source={require('../assets/images/success.png')}
         />
       </View>
       <Text style={styles.summaryText}>
-        Your phone needed <Text style={styles.hightlight}>{roundsNumber}</Text> rounds to
-        guess the number <Text style={styles.hightlight}>{userNumber}</Text>.
+        Your phone needed <Text style={styles.hightlight}>{roundsNumber}</Text>{' '}
+        rounds to guess the number{' '}
+        <Text style={styles.hightlight}>{userNumber}</Text>.
       </Text>
       <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
     </View>
+    </ScrollView>
   );
 };
 
-const deviceWidth = Dimensions.get('window').width;
+// const deviceWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
   rootContainer: {
     flex: 1,
     justifyContent: 'center',
@@ -33,9 +59,9 @@ const styles = StyleSheet.create({
     padding: 24,
   },
   imageContainer: {
-    borderRadius: deviceWidth < 380 ? 75 : 150,
-    width: deviceWidth < 380 ? 150 : 300,
-    height: deviceWidth < 380 ? 150 : 300,
+    // borderRadius: deviceWidth < 380 ? 75 : 150,
+    // width: deviceWidth < 380 ? 150 : 300,
+    // height: deviceWidth < 380 ? 150 : 300,
     overflow: 'hidden',
     borderWidth: 3,
     borderColor: Colors.primary800,
