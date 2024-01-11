19bb295e8e740da8eadf2f7987e8e6e729601d04
William Watson <williampwatson1991@gmail.com>
2024-01-10 19:54:45 -0500

    feat: added GuessLogItem dummy component to game components folder

---
+++ components/game/GuessLogItem.js
@@ -0,0 +1,9 @@
+import { View, StyleSheet } from 'react-native';
+
+const GuessLogItem = ({ guess, round }) => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default GuessLogItem;
