5df0a6010648c033781d3bf3e7ef8565a8a44b5f
William Watson <williampwatson1991@gmail.com>
2024-01-10 19:26:50 -0500

    feat: image added to game over screen. dummy text explaining what happened in the game also added

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,21 +1,45 @@
-import { Text, StyleSheet, View, Image } from 'react-native'
+import { Text, StyleSheet, View, Image } from 'react-native';
 
-import Title from '../components/ui/Title'
+import Title from '../components/ui/Title';
+import Colors from '../constants/colors';
 
 const GameOverScreen = () => {
   return (
-    <View>
+    <View style={styles.rootContainer}>
       <Title>GAME OVER!</Title>
-      <Image />
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require('../assets/images/success.png')}
+        />
+      </View>
+      <View>
+        <Text>Your phone needed X rounds to guess the number y.</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  text: {
-    color: 'white',
-    fontSize: 100,
-  }
-})
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 24,
+  },
+  imageContainer: {
+    borderRadius: 150,
+    width: 300,
+    height: 300,
+    overflow: 'hidden',
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    margin: 36,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default GameOverScreen;
