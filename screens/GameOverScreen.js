80c28de3fefca8e0b73eda97f4369f89b0799178
William Watson <williampwatson1991@gmail.com>
2024-01-11 19:09:55 -0500

    feat: demonstrating file naming conventions that react native recognizes in order to differenctiate between what file or component is being used based on platform (android, ios). Platform API also demonstrated in situations where a problem is more minor

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,10 +1,17 @@
-import { Text, StyleSheet, View, Image, useWindowDimensions, ScrollView } from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  useWindowDimensions,
+  ScrollView,
+} from 'react-native';
 
 import Title from '../components/ui/Title';
 import PrimaryButton from '../components/ui/PrimaryButton';
 import Colors from '../constants/colors';
 
-const GameOverScreen = ({roundsNumber, userNumber, onStartNewGame}) => {
+const GameOverScreen = ({ roundsNumber, userNumber, onStartNewGame }) => {
   const { width, height } = useWindowDimensions();
 
   let imageSize = 300;
@@ -13,7 +20,7 @@ const GameOverScreen = ({roundsNumber, userNumber, onStartNewGame}) => {
     imageSize = 150;
   }
 
-  if (height <400) {
+  if (height < 400) {
     imageSize = 80;
   }
 
@@ -21,27 +28,25 @@ const GameOverScreen = ({roundsNumber, userNumber, onStartNewGame}) => {
     width: imageSize,
     height: imageSize,
     borderRadius: imageSize / 2,
-  }
-
-
+  };
 
   return (
     <ScrollView style={styles.screen}>
-    <View style={styles.rootContainer}>
-      <Title>GAME OVER!</Title>
-      <View style={[styles.imageContainer, imageStyle]}>
-        <Image
-          style={styles.image}
-          source={require('../assets/images/success.png')}
-        />
+      <View style={styles.rootContainer}>
+        <Title>GAME OVER!</Title>
+        <View style={[styles.imageContainer, imageStyle]}>
+          <Image
+            style={styles.image}
+            source={require('../assets/images/success.png')}
+          />
+        </View>
+        <Text style={styles.summaryText}>
+          Your phone needed{' '}
+          <Text style={styles.hightlight}>{roundsNumber}</Text> rounds to guess
+          the number <Text style={styles.hightlight}>{userNumber}</Text>.
+        </Text>
+        <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
       </View>
-      <Text style={styles.summaryText}>
-        Your phone needed <Text style={styles.hightlight}>{roundsNumber}</Text>{' '}
-        rounds to guess the number{' '}
-        <Text style={styles.hightlight}>{userNumber}</Text>.
-      </Text>
-      <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
-    </View>
     </ScrollView>
   );
 };
